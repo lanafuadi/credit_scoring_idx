e09b44e42c161e69d1f9dbfc184fb3c1b26d5312
Lana Fuadi <lanafuadi@gmail.com>
2023-07-22 09:14:54 +0700

    3rd commit

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 import streamlit as st
-import joblib
-from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
+import pickle
+import pandas as pd
 
 # Load the trained logistic regression model
-logreg = joblib.load('logistic_regression_model.pkl')
+with open('logreg_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
 # Streamlit app code
 def main():
-    st.title('Logistic Regression Model App')
-    st.write('Enter your input features and click the "Predict" button to get predictions.')
+    st.title('Credit Scoring Prediction App')
+    st.write('Enter the feature values and click the "Predict" button to get credit scoring prediction.')
 
     # Input fields for the features required for prediction
-    feature1 = st.number_input('Feature 1', value=0.0)
-    feature2 = st.number_input('Feature 2', value=0.0)
-    # Add more input fields for other features as needed
+    last_pymnt_amnt = st.number_input('Last Payment Amount', value=0.0, step=1.0)
+    loan_amnt = st.number_input('Loan Amount', value=100.0, step=1.0)
+    int_rate = st.number_input('Interest Rate', value=10.0, step=0.01)
 
-    # Prepare the input data as a NumPy array or Pandas DataFrame
-    input_data = [[feature1, feature2]]
-    
+    # Prepare the input data as a DataFrame with the same column names as in the training data
+    input_data = pd.DataFrame({
+        'last_pymnt_amnt': [last_pymnt_amnt],
+        'loan_amnt': [loan_amnt],
+        'int_rate': [int_rate]
+        # Add more columns for other features as needed
+    })
+
     # Make predictions when the user clicks the "Predict" button
     if st.button('Predict'):
         # Perform any necessary preprocessing on the input data (e.g., scaling, encoding)
         # Make predictions using the loaded model
-        y_pred = logreg.predict(input_data)
-        # Display the prediction
-        st.write(f'Prediction: {y_pred[0]}')
+        prediction = model.predict(input_data)
+        proba = model.predict_proba(input_data)[:, 1]
+
+        # Display the prediction and probability
+        st.write(f'Prediction: {"Good" if prediction[0] == 0 else "Bad"}')
+        st.write(f'Probability of Default: {proba[0]:.4f}')
 
 if __name__ == '__main__':
     main()
