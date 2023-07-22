dfb669ec017859a0b5adf29320ce454dfcff9d98
Lana Fuadi <lanafuadi@gmail.com>
2023-07-22 10:09:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,13 +13,18 @@ def main():
 
     # Input fields for the features required for prediction
     loan_amnt = st.number_input('Loan Amount', value=100.0, step=1.0)
-    int_rate = st.number_input('Interest Rate', value=10.0, step=0.01)
+    int_rate = st.number_input('Interest Rate', value=10.0, step=0.1)
+    annual_inc = st.number_input('Annual Income', value=50000.0, step=1000.0)
+    last_pymnt_amnt = st.number_input('Last Payment Amount', value=0.0, step=1.0)
+    pymnt_time = st.number_input('Payment Time', value=1, step=1)
 
     # Prepare the input data as a DataFrame with the same column names as in the training data
     input_data = pd.DataFrame({
         'loan_amnt': [loan_amnt],
-        'int_rate': [int_rate]
-        # Add more columns for other features as needed
+        'int_rate': [int_rate],
+        'annual_inc': [annual_inc],
+        'last_pymnt_amnt': [last_pymnt_amnt],
+        'pymnt_time': [pymnt_time]
     })
 
     # Make predictions when the user clicks the "Predict" button
