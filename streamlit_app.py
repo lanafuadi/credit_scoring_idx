fc30ce37b38cfbce0e69ff9d424227e8a45af4ce
Lana Fuadi <lanafuadi@gmail.com>
2023-07-22 10:27:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,19 +12,15 @@ def main():
     st.write('Enter the feature values and click the "Predict" button to get credit scoring prediction.')
 
     # Input fields for the features required for prediction
-    loan_amnt = st.number_input('Loan Amount', value=100.0, step=1.0)
-    int_rate = st.number_input('Interest Rate', value=10.0, step=0.1)
-    annual_inc = st.number_input('Annual Income', value=50000.0, step=1000.0)
-    last_pymnt_amnt = st.number_input('Last Payment Amount', value=0.0, step=1.0)
-    pymnt_time = st.number_input('Payment Time', value=1, step=1)
+    loan_amnt_ = st.number_input('Loan Amount', value=100.0, step=1.0)
+    int_rate_ = st.number_input('Interest Rate', value=10.0, step=0.1)
+    annual_inc_ = st.number_input('Annual Income', value=50000.0, step=1000.0)
 
     # Prepare the input data as a DataFrame with the same column names as in the training data
     input_data = pd.DataFrame({
-        'loan_amnt': [loan_amnt],
-        'int_rate': [int_rate],
-        'annual_inc': [annual_inc],
-        'last_pymnt_amnt': [last_pymnt_amnt],
-        'pymnt_time': [pymnt_time]
+        'loan_amnt_': [loan_amnt_],
+        'int_rate_': [int_rate_],
+        'annual_inc_': [annual_inc_]
     })
 
     # Make predictions when the user clicks the "Predict" button
@@ -35,7 +31,7 @@ def main():
         proba = model.predict_proba(input_data)[:, 1]
 
         # Display the prediction and probability
-        st.write(f'Prediction: {"Good" if prediction[0] == 0 else "Bad"}')
+        st.write(f'Prediction: {"Low Default Risk" if prediction[0] == 0 else "High Default Risk"}')
         st.write(f'Probability of Default: {proba[0]:.4f}')
 
 if __name__ == '__main__':
